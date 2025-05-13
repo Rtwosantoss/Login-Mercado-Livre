8b3f7533c5100f3ecbc5ab46399ce4f5a0bf2d42
auauauau <auauauau@gmail.com>
2025-05-12 22:23:27 -0300

    tela login inicial

---
+++ Tela login/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Footer from "./components/FooterComponents.vue";
+
+</script>
+
+<template>
+
+<div class="flex flex-col min-h-screen bg-gray-100">
+   <div class="main container mx-auto px-4 py-6">
+      <router-view> </router-view>
+    </div>
+
+    <Footer />
+</div>
+
+</template>
---
+++ Tela login/src/components/FooterComponents.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer
+    class="fixed bottom-0 w-full bg-blue-600 text-white py-6 text-center mt-12">
+  <div class="mt-5 flex text-center text-sm text-gray-400">
+      <p>
+        This site is protected by reCAPTCHA and the Google <br />
+        <a class="underline" href="">Privacy Policy</a>  and <a class="underline" href="">Terms of Service</a>  apply.
+      </p>
+    </div>
+  </footer>
+</template>
